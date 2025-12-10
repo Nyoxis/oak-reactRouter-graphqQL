aedfcba8d913c96b4552bed4dd061c109526a153
Nyoxis <omicronnyoxis@gmail.com>
2025-12-10 11:59:56 +0200

    chore: minor fixes for demo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./drizzle/schema.ts",
+  out: "./api/drizzle",
+  schema: "./api/drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: Deno.env.get("DATABASE_URL")!,
